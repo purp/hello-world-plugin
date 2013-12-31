cae0041a135523db7a14356ca6ab040faf171b70
Jim Meyer <jim@geekdaily.org>
2013-12-31 15:52:34 -0600

    Amend startup script

--- start_bukkit.sh
+++ start_bukkit.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cd "$( dirname "$0" )"
-# java -Xmx1024M -jar craftbukkit.jar -o true
-java -Xms1024M -Xmx1024M -jar craftbukkit.jar -o true
+cd local-server && java -Xms1024M -Xmx1024M -jar craftbukkit.jar -o true
 
