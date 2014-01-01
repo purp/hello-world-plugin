5f1d67bc78426e51e66666bf66281acf05634214
Jim Meyer <jim@geekdaily.org>
2014-01-01 13:56:56 -0600

    Start inside a named screen session

--- start_bukkit.sh
+++ start_bukkit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd local-server && java -Xms1024M -Xmx1024M -jar craftbukkit.jar -o true
+# cd local-server && ! screen -list | grep -q craftbukkit_server && screen -d -S craftbukkit_server -m java -Xms1024M -Xmx1024M -jar craftbukkit.jar -o true
+# TODO: get jar name from config
+cd local-server && screen -d -S craftbukkit_server -m java -Xms1024M -Xmx1024M -jar craftbukkit-dev.jar -o true
 
